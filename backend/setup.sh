ec6fa59a3e616ab63fe6a404f81938a9dc8e2a5a
AdityaAgg <agga140@usc.edu>
2019-04-13 00:39:00 -0700

    travis configuration issue?

--- backend/setup.sh
+++ backend/setup.sh
@@ -2,5 +2,5 @@
 pip3 install --upgrade pip
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $DIR/.env
-sudo pip3 install -r $DIR/requirements.txt
-sudo pip3 install -U pytest
+sudo pip install -r $DIR/requirements.txt
+sudo pip install -U pytest
