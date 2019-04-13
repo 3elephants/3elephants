452224fc47220a26eb6dcee5b9cfa60788a8d918
AdityaAgg <agga140@usc.edu>
2019-04-13 00:41:17 -0700

    dont upgrade pip

--- backend/setup.sh
+++ backend/setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-pip3 install --upgrade pip
+#pip3 install --upgrade pip
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $DIR/.env
 sudo pip install -r $DIR/requirements.txt
