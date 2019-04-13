ba865df621ec6060c7cdf83f08a621872fd1001c
AdityaAgg <agga140@usc.edu>
2019-04-13 00:33:01 -0700

    pip3 the issue?

--- backend/setup.sh
+++ backend/setup.sh
@@ -2,5 +2,5 @@
 pip3 install --upgrade pip
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $DIR/.env
-sudo pip install -r $DIR/requirements.txt
-sudo pip install -U pytest
+sudo pip3 install -r $DIR/requirements.txt
+sudo pip3 install -U pytest
