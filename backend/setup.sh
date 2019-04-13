be5af0247a10c5563f7b379b904167af67273383
AdityaAgg <agga140@usc.edu>
2019-04-13 01:16:36 -0700

    why is it defaulting to 2.7?

--- backend/setup.sh
+++ backend/setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $DIR/.env
-sudo pip install -r $DIR/requirements.txt
-sudo pip install -U pytest
+pip install -r $DIR/requirements.txt
+pip install -U pytest
